38c39bfd8bc17f7eaa909ff06f27b0b7e80f091c
LucasMMBB <maoxummbb@gmail.com>
2017-04-11 14:25:35 -0400

    update two methods for question sort colors

--- leetcode.java
+++ leetcode.java
@@ -1,6 +1,8 @@
 /*\
 |*| Leetcode answers
 |*| Author: Maoxu
+|*| Description: share my answers to whoever needs
+|*| Create date: April 3rd, 2017
 \*/
 // 71. Simplify path
 public class Solution {
@@ -30,3 +32,139 @@ public class Solution {
 }
 
 // 84. Largest Rectangle in Histogram
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+
+        if(heights == null || heights.length == 0){return 0;}
+
+        Deque<Integer> stack = new LinkedList<Integer>();//store the index
+        int max = 0;
+
+        for(int i=0;i<=heights.length;i++){
+        	//Each elem will be push once and poll once
+        	//1. Check whether this elem can be pushed into the stack
+        	int curVal = i == heights.length?0:heights[i];
+        	while(!stack.isEmpty() && heights[stack.peekLast()]>=curVal){
+        		int height = heights[stack.pollLast()];
+        		int leftBound = stack.isEmpty()?0:stack.peekLast() + 1;
+        		int rightBound = i;
+        		max = Math.max(max, height*(rightBound - leftBound));
+        	}
+
+        	//2. Push the elem into the stack
+        	stack.addLast(i);
+        }
+
+        return max;
+
+    }
+}
+
+// 42. Trapping rain water
+public class Solution {
+    public int trap(int[] arr) {
+    	if(arr == null || arr.length <=2 ) {return 0;}
+
+    	//Two scanners
+    	int left = 0;
+    	int right = arr.length - 1;
+    	int sum = 0;
+
+    	//Two walls
+    	int leftMax = 0;
+    	int rightMax = 0;
+    	while(left<=right){
+    		//Move lower wall first: Guarantee middle region can trap water
+    		if(leftMax<=rightMax){
+    			leftMax = Math.max(leftMax, arr[left]);
+	    		if(arr[left]<leftMax){
+	    			sum+=leftMax - arr[left];
+	    			left++;
+	    		}
+    		}else{
+    			rightMax = Math.max(rightMax, arr[right]);
+    			if(arr[right]<rightMax){
+    				sum += rightMax - arr[right];
+    				right--
+    			}
+    		}
+    	}
+    	return sum;
+	}
+}
+
+
+// 75. sort colors
+public class Solution {
+    public void sortColors(int[] nums) {
+	    if(nums == null || nums.length <= 1){
+		  return;    
+	      //System.out.println("fuck you!!");
+		}
+	    int lb = 0;
+	    int rb = nums.length - 1;
+	    int i = 0; // scanner index
+	    while(i<=rb){
+	      if(nums[i]==2){
+	      	swag(nums, i, rb);
+	        rb-=1;
+	      }else if(nums[i]==0){
+	      	swag(nums, i, lb);
+	        lb+=1;
+	        i+=1;
+	      }else{
+	      	i+=1;
+	      }
+	    }
+    }// sortCOlors ends
+
+ 	public void sortColors_M2(int[] nums) {
+	    if(nums == null || nums.length <= 1){
+	      System.out.println("fuck you!!");
+	    }
+	    
+	    int countzero = 0;
+	    int countone = 0;
+	    int counttwo = 0;
+	    
+	    for(int i = 0;i<nums.length;i++){
+	      if(nums[i]==0){
+	      	countzero+=1;
+	      }else if(nums[i]==1){
+	      	countone+=1;
+	      }else{
+	      	counttwo+=1;
+	      }
+	    }
+	    
+	    for(int j=0;j<nums.length;j++){
+	      if(j<countzero){
+	      	nums[j] = 0;
+	      }else if(j>=countzero && j<(countzero+countone)){
+	      	nums[j] = 1;
+	      }else{
+	      	nums[j] = 2;
+	      }
+	    }
+    
+    	printArray(nums);
+    }   
+
+    private void printArray(int[] anArray) {
+      for (int i = 0; i < anArray.length; i++) {
+         if (i > 0) {
+            System.out.print(", ");
+         }
+         System.out.print(anArray[i]);
+      }
+   }
+
+    private void swag(int[] arr, int a, int b){
+	    if(arr[a]!=arr[b]){
+	   		int temp = arr[b];
+	      	arr[b]=arr[a];
+	      	arr[a]=temp;
+	    }
+  }
+
+}
