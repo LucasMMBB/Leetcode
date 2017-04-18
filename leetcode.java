854a1177f2df81573a31027d1cbd337667f1e783
LucasMMBB <maoxummbb@gmail.com>
2017-04-18 13:07:33 -0400

    Finish question Number of 1 Bits

--- leetcode.java
+++ leetcode.java
@@ -549,6 +549,21 @@ public class Solution {
 }
 
 // 191. Number of 1 Bits
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        if(n==0){return 0;}
+        int count = 0;
+        for(int i=0;i<32;i++){
+            if( (n&1) == 1){
+                count++;
+            }
+            n>>>=1;
+        }
+        return count;        
+    }
+}
+
 
 // 50. Pow(x,n)
 public class Solution {
