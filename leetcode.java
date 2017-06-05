fdd84afd2e2257ace70d8ca5ac576c32b0c89c7f
LucasMMBB <maoxummbb@gmail.com>
2017-06-04 22:57:51 -0400

    Question 153

--- leetcode.java
+++ leetcode.java
@@ -677,3 +677,36 @@ public class Solution {
         return false;
     }
 }
+
+// 153. Find Minimum in Rotated Sorted Array
+// Regular: O(Log(n))
+public class Solution {
+    public int findMin(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start + 1 < end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] > nums[start]){
+                if(nums[mid]<nums[end]){
+                    return nums[start];
+                }else{
+                    start = mid;
+                }
+            }else if(nums[mid] < nums[start]){
+                end = mid;
+            }else{
+                start ++;// duplicates
+            }
+        }
+        if(nums[start] <=  nums[end]){
+            return nums[start];
+        }
+        else{
+            return nums[end];
+        }
+    }
+}
