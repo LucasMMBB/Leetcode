424dd1866cc5112cf3cc9168acef1ed1495ba846
LucasMMBB <maoxummbb@gmail.com>
2017-06-04 22:19:15 -0400

    add question 33

--- leetcode.java
+++ leetcode.java
@@ -591,3 +591,51 @@ public class Solution {
         
     }
 }
+
+// 33. Search in Rotated Sorted Array
+public class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return -1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target){
+        if(nums == null || nums.length == 0)
+            return -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start + 1 < end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[mid] > nums[start]){
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid;
+                }else{
+                    start = mid;
+                }
+            }else if(nums[mid] < nums[start]){
+                if(target >= nums[mid] && target <= nums[end]){
+                    start = mid;
+                }else{
+                    end = mid;
+                }
+            }else{
+                start ++;// duplicates
+            }
+        }
+        if(nums[start] == target)
+            return start;
+        if(nums[end] ==  target)
+            return end;
+        return -1;
+    }// method ends
+}
