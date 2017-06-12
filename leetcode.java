4de5ad1186ceb2935c85a3f0f074bac987cfd821
LucasMMBB <maoxummbb@gmail.com>
2017-06-12 19:01:01 -0400

    finish question Unique Paths

--- leetcode.java
+++ leetcode.java
@@ -1141,8 +1141,24 @@ public class Solution {
 
 
 // 62. Unique Paths
+/**
+ * Dynamic programming
+ * Time complexity: O(mn); space: O(mn)
+ */
 public class Solution {
     public int uniquePaths(int m, int n) {
-        
+        if(m == 1 || n == 1)
+            return 1;
+        int[][] res = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0){
+                    res[i][j] = 1;
+                }else{
+                    res[i][j] = res[i-1][j] + res[i][j-1];
+                }
+            }
+        }
+        return res[m-1][n-1];
     }
 }
