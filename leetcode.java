bdc36a212e6e3802c9f01390931fdb76e5f1b35a
LucasMMBB <maoxummbb@gmail.com>
2017-06-14 17:32:07 -0400

    finish question Missing Number

--- leetcode.java
+++ leetcode.java
@@ -1399,3 +1399,25 @@ public class Solution {
         }
     }
 }
+
+// 287. Find the Duplicate Number
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if(nums.length > 1){
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (slow != fast){
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+
+            fast = 0;
+            while(fast != slow){
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
+        }// if ends
+        return -1;
+    }
+}
