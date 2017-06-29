494b27185f0a1673d7eacead341c655505db8464
LucasMMBB <maoxummbb@gmail.com>
2017-06-29 14:03:27 -0400

    finish question 3Sum closer

--- leetcode.java
+++ leetcode.java
@@ -1635,5 +1635,28 @@ public class Solution {
 public class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         
+        int size = nums.length;
+        if(nums == null || nums.length < 3)
+        	return 0;
+
+        Arrays.sort(nums);
+        int res = 0;
+        for(int i = 0; i < size; i++){
+        	int left = i + 1;
+        	int right = size - 1;
+
+        	while(left < right){
+        		int sum = nums[i] + nums[left] + nums[right];
+
+        		if(sum <  target){
+        			res += right - left;
+        			left++;
+        		}else{
+        			right--;
+        		}
+
+        	}//while ends
+        }// for ends
+        return res;
     }
 }
