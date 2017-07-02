b6424034290d8714fdfbce550eae8419ae3fb518
LucasMMBB <maoxummbb@gmail.com>
2017-07-02 15:17:03 -0400

    add python solution for question MERGE SORTED ARRAY

--- leetpython.py
+++ leetpython.py
@@ -28,3 +28,28 @@ class Solution(object):
 
         		if haystack[i+j] != needle[j]:
         			break
+
+    # 88. Merge Sorted Array
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1;
+        j = n - 1;
+        k = m + n - 1;
+        while k >= 0 and i >= 0 and j >= 0:
+        	if nums1[i] > nums2[j]:
+        		nums1[k] = nums1[i]
+        		i -= 1
+        	else:
+        		nums1[k] = nums2[j]
+        		j -= 1
+
+        	k -= 1
+
+        if j >= 0:
+        	nums1[:j+1] = nums2[:j+1]
