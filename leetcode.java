4204f249a27c7e34c174e48fcd1bb9bd7e2273b9
LucasMMBB <maoxummbb@gmail.com>
2017-07-01 22:26:56 -0400

    init question Merge Intervals

--- leetcode.java
+++ leetcode.java
@@ -1869,3 +1869,20 @@ public class Solution {
     	// method3
     }
 }
+
+
+// 56. Merge Intervals
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        
+    }
+}
