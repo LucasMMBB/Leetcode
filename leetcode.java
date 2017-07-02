edc0db04369566ccb11a164c0510f90dca497eaa
LucasMMBB <maoxummbb@gmail.com>
2017-07-02 00:26:53 -0400

    add quick select function

--- leetcode.java
+++ leetcode.java
@@ -1852,6 +1852,7 @@ class Solution(object):
  * Time complexity: O(NlogN), space complexity: O(1)
  * Method2: Other possibility is to use a min oriented priority queue that will
  * store the k-th largest values.
+ * Method 3: quick select
  */
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
@@ -1864,16 +1865,50 @@ public class Solution {
     	// method 2
     	final PriorityQueue<Integer> pq = new PriorityQueue<>();
     	for(int val : nums){
-    		pq.off(val);
+    		pq.offer(val);
 
-    		if(pq.size() > )
+    		if(pq.size() > k){
+    			pq.poll();
+    		}
     	}// for ends
+    	return pq.peek();
 
     }
 
-    public int findKthLargest_m3(int[] nums, int k){
-    	// method3
+  public int findKthLargest_m3(int[] a, int k) {
+    int n = a.length;
+    int p = quickSelect(a, 0, n - 1, n - k + 1);
+    return a[p];
+  }
+  
+  // return the index of the kth smallest number
+  public int quickSelect(int[] a, int lo, int hi, int k) {
+    // use quick sort's idea
+    // put nums that are <= pivot to the left
+    // put nums that are  > pivot to the right
+    int i = lo, j = hi, pivot = a[hi];
+    while (i < j) {
+      if (a[i++] > pivot) swap(a, --i, --j);
     }
+    swap(a, i, hi);
+    
+    // count the nums that are <= pivot from lo
+    int m = i - lo + 1;
+    
+    // pivot is the one!
+    if (m == k)     return i;
+    // pivot is too big, so it must be on the left
+    else if (m > k) return quickSelect(a, lo, i - 1, k);
+    // pivot is too small, so it must be on the right
+    else            return quickSelect(a, i + 1, hi, k - m);
+  }
+  
+  public void swap(int[] a, int i, int j) {
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
+
 }
 
 
