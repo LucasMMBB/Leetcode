b4e9420ab7012704773d58265e107b9e9f60b9b5
LucasMMBB <maoxummbb@gmail.com>
2017-07-02 13:17:46 -0400

    add python solution for IMPLEMENT STRSTR

--- leetpython.py
+++ leetpython.py
@@ -6,3 +6,25 @@
 |*| Create date: April 3rd, 2017
 \*/
 """
+class Solution(object):
+	# 28. Implement strStr
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        m = len(haystack)
+        n = len(needle)
+
+        for i in range(m+1):
+        	for j in range(n+1):
+
+        		if j == n:
+        			return i
+
+        		if i + j == m:
+        			return -1
+
+        		if haystack[i+j] != needle[j]:
+        			break
