163a43b6bf1490bc3e2e01f7c260dbfd8a8435e0
LucasMMBB <maoxummbb@gmail.com>
2017-07-04 17:50:59 -0400

    started to work on LINKED LIST questions

--- leetcode.java
+++ leetcode.java
@@ -2093,6 +2093,53 @@ public class Solution {
 // 214. Shortest Palindrome
 public class Solution {
     public String shortestPalindrome(String s) {
-        
+
+    }
+}
+
+//21. Merge Two Sorted Lists
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+        	return l2;
+        if(l2 == null)
+        	return l1;
+
+        ListNode mergeHead;
+        if(l1.val < l2.val){
+        	mergeHead = l1;
+        	mergeHead.next = mergeTwoLists(l1.next, l2);
+        }else{
+        	mergeHead = l2;
+        	mergeHead.next = mergeTwoLists(l1, l2.next);
+        }//if ends
+        return mergeHead;
+    }
+
+    // some basic methods for linked list
+    public ListNode mergeTwoLists_m2(ListNode l1, ListNode l2){
+        if(l1 == null)
+        	return l2;
+        if(l2 == null)
+        	return l1;
+
+        ListNode mergeHead;
+        while(l1 != null || l2 != null){
+        	if(l1.val < l2.val){
+        		mergeHead = l1;
+        		l1 = l1.next; 
+        	}else{
+        		mergeHead = l2;
+        		l2 = l2.next;
+        	}//if ends
+        }//while ends
     }
 }
