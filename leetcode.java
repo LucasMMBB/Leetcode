93c7edda3a7fe4a423cbf0fabda5621d3a4a5ed5
LucasMMBB <maoxummbb@gmail.com>
2017-07-05 16:51:17 -0400

    review question PALINDROME NUMBER

--- leetcode.java
+++ leetcode.java
@@ -2317,3 +2317,20 @@ public class Solution {
     	}// while ends
     }
 }
+
+// 9. Palindrome Number
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x != 0 && x%10 == 0))
+        	return false;
+
+        int rev = 0;
+        while(x > rev){
+        	rev = rev*10 + x%10;
+        	x = x/10;
+        }// while ends
+
+        return (x == rev || x == rev/10);
+
+    }
+}
