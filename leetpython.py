fd33121f262078a399581a1262201ffa6a6b1a1b
LucasMMBB <maoxummbb@gmail.com>
2017-07-06 19:36:19 -0400

    init question REVERSE LINKED LIST ||

--- leetpython.py
+++ leetpython.py
@@ -203,3 +203,15 @@ class Solution(object):
      		h2 = h2.next
 
      	return True
+
+# 92. Reverse Linked List
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+    	dummy = head
+    	left, right = m, n
