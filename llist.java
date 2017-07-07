ce21d376e8843ddfea4f27442a4da825a1c2763d
LucasMMBB <maoxummbb@gmail.com>
2017-07-06 23:50:17 -0400

    init LINKEDLIST class in java

--- llist.java
+++ llist.java
@@ -1,76 +1,40 @@
-public class ReverseLinkedList{
-	public static void main (String[] args) throws java.lang.Exception{
-		LinkedListT a = new LinkedListT();
-		a.addAtBegin(5);
-		a.addAtBegin(10);
-		a.addAtBegin(15);
-		a.addAtBegin(20);
-	}
-}
+// A simple Java program for traversal of a linked list
+public class LinkedList
+{
+	Node head; // head of list
 
-class Node{
-	public int data;
-	public Node next;
-	public Node(int data){
-		this.val = data;
-		this.next = null;
-	}
-}
-
-class LinkedListT{
-	public LinkedListT(){
-		head = null;
+	/* Linked list Node. This inner class is made static so that
+	main() can access it */
+	static class Node {
+		int data;
+		Node next;
+		Node(int d) { data = d; next=null; } // Constructor
 	}
 
-	public void addAtBegin(int data){
-		Node n = new Node(data);
-		n.next = head;
-		head = n;
-	}
-
-	public void reverseIterative(Node head){
-		Node currNode = head;
-		Node nextNode = null;
-		Node prevNode = null;
-
-		while(currNode != null){
-			nextNode = currNode.next;
-			currNode.next = prevNode;
-			prevNode = currNode;
-			currNode = nextNode;
-		}// while ends
-		head = prevNode;
-		System.out.println("\n Reverse through iteration");
-		display(head);
-	}
-
-	public void reverseRecursion(Node ptrOne,Node ptrTwo, Node prevNode){
-		if(ptrTwo!=null){
-				if(ptrTwo.next!=null){
-					Node t1 = ptrTwo;
-					Node t2 = ptrTwo.next;
-					ptrOne.next = prevNode;
-					prevNode = ptrOne;
-					reverseRecursion(t1,t2, prevNode);
-				}
-				else{
-					ptrTwo.next = ptrOne;
-					ptrOne.next = prevNode;
-					System.out.println("\n Reverse Through Recursion");
-					display(ptrTwo);
-				}
+	/* This function prints contents of linked list starting from head */
+	public void printList()
+	{
+		Node n = head;
+		while (n != null)
+		{
+			System.out.print(n.data+" ");
+			n = n.next;
 		}
-		else if(ptrOne!=null){
-			System.out.println("\n Reverse Through Recursion");
-			display(ptrOne);
-		}
-	}// reverseRecursion ends
+	}
 
-	public void display(Node head){
-		Node cur = head;
-		while(cur != null){
-			System.out.print("->" + cur.data);
-			cur = cur.data;
-		}
-	}// display ends
+	/* method to create a simple linked list with 3 nodes*/
+	public static void main(String[] args)
+	{
+		/* Start with the empty list. */
+		LinkedList llist = new LinkedList();
+
+		llist.head	 = new Node(1);
+		Node second	 = new Node(2);
+		Node third	 = new Node(3);
+
+		llist.head.next = second; // Link first node with the second node
+		second.next = third; // Link first node with the second node
+
+		llist.printList();
+	}
 }
