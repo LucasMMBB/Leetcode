77c932c026f53b0fcdf87714b0cc91bba272793a
LucasMMBB <maoxummbb@gmail.com>
2017-07-14 22:11:08 -0400

    update a better method for MAXIMUM PRODUCT OF THREE NUMBERS

--- leetcode.java
+++ leetcode.java
@@ -2801,6 +2801,33 @@ public class Solution {
         int a2 = nums[0] * nums[1] * nums[nums.length - 1];
         return Math.max(a1, a2);
     }
+
+    public int maximumProduct_m2(int[] nums){
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        for(int n: nums){
+            // min1 < min2
+            if(n <= min1){
+                min2 = min1;
+                min1 = n;
+            } else if (n <= min2){
+                min2 = n;
+            }
+
+            // max1 > max2 > max3
+            if(n > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            }else if( n >= max2){
+                max3 = max2;
+                max2 = n;
+            }else if(n >= max3){
+                max3 = n;
+            }
+        }// for ends
+        return Math.max(min1*min2*max1, max3*max2*max1);
+    }
 }
 // 222. Count Complete Tree Nodes
 /**
