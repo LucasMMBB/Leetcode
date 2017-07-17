80cf1584d497b4eb3a0c27caf78df74de550d7fb
LucasMMBB <maoxummbb@gmail.com>
2017-07-16 23:27:29 -0400

    finished question CONTAINS DUPLICATE in python

--- leetpython.py
+++ leetpython.py
@@ -545,6 +545,8 @@ class Solution(object):
         a = nums[-1] * nums[-2] * nums[-3]
         b = nums[0] * nums[1] * nums[-1]
         return max(a,b)
+
+
 # 35. Search Insert Position
 class Solution(object):
     def searchInsert(self, nums, target):
@@ -563,7 +565,23 @@ class Solution(object):
             else:
                 r = mid - 1
         return l        
+
+
 # 217. Contains Duplicate
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        pyset = set()
+        for i in nums:
+            if i not in pyset:
+                pyset.add(i)
+            else:
+                return True
+        return False
+
 
 # 222. Count Complete Tree Nodes
 class Solution(object):
