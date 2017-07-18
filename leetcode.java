054b3c3a6c56ec372a1db795f91d2b7e51a90766
LucasMMBB <maoxummbb@gmail.com>
2017-07-18 00:45:07 -0400

    finished java solution for MINIMUM DEPTH OF BFS

--- leetcode.java
+++ leetcode.java
@@ -2911,24 +2911,27 @@ public class Solution {
 public class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        int level = 1;
-
-        while(!queue.isEmpty()){
-            for(int i = 0; i < queue.size(); i++){
-                TreeNode node = queue.poll();
-
-                if(node.left != null)
-                    queue.add(node.left);
-                if(node.right != null)
-                    queue.add(node.right);
-                if(node.left == null && node.right == null)
-                    return level;
-            }// for ends
-            level++;
-        }// while ends
-        return level;
+        int depth = 1;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            // for each level
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right == null){
+                    return depth;
+                }
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 }
 // 220. Contains Duplicate |||
