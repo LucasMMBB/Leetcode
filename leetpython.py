3269c6886bc08281be6a8388fb3cca089665632e
LucasMMBB <maoxummbb@gmail.com>
2017-07-25 23:46:54 -0400

    regular commit

--- leetpython.py
+++ leetpython.py
@@ -1480,6 +1480,14 @@ class Solution(object):
                 return False
         return True 
 
+# 349. Intersection of Two Arrays
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
 # 204. Count Primes
 class Solution(object):
     def countPrimes(self, n):
