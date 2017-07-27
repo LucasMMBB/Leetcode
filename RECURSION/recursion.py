d9ae5fc64318f64cea8560df9561aeeb708b32c6
LucasMMBB <maoxummbb@gmail.com>
2017-07-27 17:56:42 -0400

    finished RECURSION EXPLANATION EXAMPLE

--- RECURSION/recursion.py
+++ RECURSION/recursion.py
@@ -1,6 +1,8 @@
 # RECURSION TUTORIAL
 # Author: MAOXU LIU
 
+
+## ---------- Basic Example --------
 def fact(n):
 	if n <= 1:
 		return 1
@@ -8,5 +10,21 @@ def fact(n):
 		return n*fact(n - 1)
 
 
+## Stack Overflow error occurs in recursion
+def fact_of(n):
+	if n == 100:
+		return 1
+	else:
+		return n * fact_of(n - 1)
+
+## direct and indirect recursion
+def directFun():
+	directFun()
+
+def indirectFun_1():
+	indirectFun_2()
+
+def indirectFun_2():
+	indirectFun_1()
 #----------- TEST CODE -----------
 print fact(3)
