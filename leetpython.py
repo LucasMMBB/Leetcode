ec2017abdda616d7840c340f0f25b1b306bd399f
LucasMMBB <maoxummbb@gmail.com>
2017-09-11 12:47:30 -0400

    added one solution to question DECODE WAYS

--- leetpython.py
+++ leetpython.py
@@ -3473,6 +3473,35 @@ class Solution(object):
        			return valid
        		level = {s[:i] + s[i+1:] for s in level for i in range(len(s))}
 
+
+# 91. Decode Ways
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        if not s or n == 0:
+        	return 0
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        if int(s[0]) != 0:
+        	dp[1] = 1
+
+        for i in range(2, n + 1):
+        	if int(s[i - 1]) != 0:
+        		dp[i] += dp[i - 1]
+
+        	two = int(s[i-2] + s[i-1])
+        	if two >= 10 and two <= 26:
+        		dp[i] += dp[i - 2]
+
+        return dp[n]
+
+
 #-------------- TO DO LIST -----------------
 # 371. Sum of Two Integers
 class Solution(object):
