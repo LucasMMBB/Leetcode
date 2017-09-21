594f7b48f03b2dd183da3ce80f1ba9a3047cb97f
LucasMMBB <maoxummbb@gmail.com>
2017-09-20 23:59:38 -0400

    init question meeting room 2

--- leetpython.py
+++ leetpython.py
@@ -3884,6 +3884,20 @@ class Solution(object):
         """
         
 
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        
+
 #-------------- TO DO LIST -----------------
 
 # 307. Range Sum Query - Mutable
