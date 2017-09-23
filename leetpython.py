ffda361480e3848c8d0960f022e8522ad4150179
LucasMMBB <maoxummbb@gmail.com>
2017-09-23 13:54:27 -0400

    set up data structures for question integer to roman

--- leetpython.py
+++ leetpython.py
@@ -4024,6 +4024,58 @@ class Solution(object):
                 num += romanNum[s[i]]
 
         return num + romanNum[s[-1]]
+
+# 12. Integer to Roman
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        thousand = {
+            1: 'M',
+            2: 'MM',
+            3: 'MMM'
+        }
+
+        hundred = {
+            1: 'C',
+            2: 'CC',
+            3: 'CCC',
+            4: 'CD',
+            5: 'D',
+            6: 'DC',
+            7: 'DCC',
+            8: 'DCCC',
+            9: 'CM'
+        }
+
+        ten = {
+            1: 'X',
+            2: 'XX',
+            3: 'XXX',
+            4: 'XL',
+            5: 'L',
+            6: 'LX',
+            7: 'LXX',
+            8: 'LXXX',
+            9: 'CX'
+        }
+
+        one = {
+            1: 'I',
+            2: 'II',
+            3: 'III',
+            4: 'IV',
+            5: 'V',
+            6: 'VI',
+            7: 'VII',
+            8: 'VIII',
+            9: 'IX'
+        }
+
+        rm = ''
+
 #-------------- TO DO LIST -----------------
 
 # 307. Range Sum Query - Mutable
