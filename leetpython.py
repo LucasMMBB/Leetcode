c197a4f180ca86e9f6a46694a0d1a5cbefef810e
LucasMMBB <maoxummbb@gmail.com>
2017-09-23 00:59:41 -0400

    finished question MERGE INTERVALS

--- leetpython.py
+++ leetpython.py
@@ -3936,6 +3936,28 @@ class Solution(object):
                 e += 1
 
         return numRooms
+
+# 56. Merge Intervals
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """            
+        out = []
+        intervals.sort(key = lambda x : x.start)
+        for i in intervals:
+            if out and i.start <= out[-1].end:
+                out[-1].end = max(out[-1].end, i.end)
+            else:
+                out.append(i)
+        return out
 #-------------- TO DO LIST -----------------
 
 # 307. Range Sum Query - Mutable
