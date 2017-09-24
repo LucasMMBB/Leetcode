e1a374434623cadc48ca04489dfdea5398356055
LucasMMBB <maoxummbb@gmail.com>
2017-09-24 10:55:36 -0400

    added another two solutions for question INTEGER TO ROMAN

--- leetpython.py
+++ leetpython.py
@@ -4092,6 +4092,64 @@ class Solution(object):
                     return rm
             num = b
         return rm
+     def intToRoman_m2(self, num):
+        matrix = {
+            1000:{
+                1: 'M',
+                2: 'MM',
+                3: 'MMM'
+            },
+            100:{
+                1: 'C',
+                2: 'CC',
+                3: 'CCC',
+                4: 'CD',
+                5: 'D',
+                6: 'DC',
+                7: 'DCC',
+                8: 'DCCC',
+                9: 'CM'
+            },
+            10:{
+                1: 'X',
+                2: 'XX',
+                3: 'XXX',
+                4: 'XL',
+                5: 'L',
+                6: 'LX',
+                7: 'LXX',
+                8: 'LXXX',
+                9: 'XC'
+            },
+            1:{
+                1: 'I',
+                2: 'II',
+                3: 'III',
+                4: 'IV',
+                5: 'V',
+                6: 'VI',
+                7: 'VII',
+                8: 'VIII',
+                9: 'IX'
+            }
+        } # matrix ends
+        rm = ''
+        for i in [1000, 100, 10, 1]:
+            a = num / i
+            b = num % i
+
+            if a != 0:
+                rm += matrix[i][a]
+
+            num = b
+        return rm
+        
+    def intToRoman(self, num):
+        M = ["", "M", "MM", "MMM"];
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
 
 #-------------- TO DO LIST -----------------
 
