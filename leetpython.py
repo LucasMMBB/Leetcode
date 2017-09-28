b5d31361f8c92053471f9323d4aae9f2542cd56e
LucasMMBB <maoxummbb@gmail.com>
2017-09-27 20:29:06 -0400

    regular update

--- leetpython.py
+++ leetpython.py
@@ -4417,7 +4417,41 @@ class Solution(object):
                 total += 1
 
         return total
-         
+
+# 161. One Edit Distance
+class Solution(object):
+    def isOneEditDistance(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        m, n = len(s), len(t)
+        if abs(m - n) > 1:
+            return False
+        count = 0
+        i = 0
+        j = 0
+        while i < m and j < n:
+            if s[i].lower() != t[j].lower():
+                if count == 1:
+                    return False
+                else:
+                    if m > n:
+                        i += 1
+                    elif m < n:
+                        j += 1
+                    else:
+                        i += 1
+                        j += 1
+                    count += 1
+            else:
+                i += 1
+                j += 1
+        if i < m or j < n:
+            count += 1
+            
+        return count == 1
 
 #-------------- TO DO LIST -----------------
 
