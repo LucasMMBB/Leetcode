bf49933a70540a0929706089abda849fb2a7b807
LucasMMBB <maoxummbb@gmail.com>
2017-09-29 22:56:03 -0400

    added one recursive solution to question Sum of Left Leaves

--- leetpython.py
+++ leetpython.py
@@ -4549,6 +4549,30 @@ class Solution(object):
     	if not node:
     		return 0
         return int(node.val==sum)+self.helper(node.left, sum-node.val) + self.helper(node.right, sum-node.val)
+
+
+
+# 404. Sum of Left Leaves
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+        	return 0
+        if root.left and not root.left.left and not root.left.right:
+        	return root.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+
+
 #-------------- TO DO LIST -----------------
 
 # 76. Minimum Window Substring
