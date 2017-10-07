29f6118540a010ae08383941b94716ec410fb98e
LucasMMBB <maoxummbb@gmail.com>
2017-10-07 19:55:22 -0400

    update the Selection Sort

--- Sort/sort.py
+++ Sort/sort.py
@@ -1,3 +1,19 @@
+# SORT METHODS
+## --------- Selection Sort ---------
+class Sort(object):
+	def __init__(self, nums):
+		self.arr = nums
+
+	def selectionSort(self):
+		# time: O(n^2), space: 
+		for i in range(len(self.arr)):
+			pivot = self.arr[i]
+			for j in range(i+1, len(self.arr)):
+				if self.arr[j] < pivot:
+					self.arr[j], self.arr[i] = self.arr[i], self.arr[j]
+
+		return self.arr
+
 # 347. Top K Frequent Elements
 class Solution(object):
     def topKFrequent(self, nums, k):
@@ -14,6 +30,7 @@ class Solution(object):
         for num in nums:
             if num in hashmap:
                 hashmap[num] += 1
+
             else:
                 hashmap[num] = 1
         for key in hashmap:
