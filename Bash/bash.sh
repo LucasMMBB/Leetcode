9737bfb2c563840d3afb1a8b9ee72ee3ea454ca5
LucasMMBB <maoxummbb@gmail.com>
2017-10-29 20:35:59 -0400

    add solutions to question Tenth Line

--- Bash/bash.sh
+++ Bash/bash.sh
@@ -1,2 +1,11 @@
 # 195. Tenth Line
 # Read from the file file.txt and output the tenth line to stdout.
+
+# solution 1
+awk 'NR'=='10' file.txt
+
+# soution 2
+sed -n 10p file.txt
+
+# soutin 3
+tail -n+10 file.txt|head-1
