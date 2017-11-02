1aa9109d55c2a18a9f32fd8f5c1d1d3a0fb24aba
LucasMMBB <maoxummbb@gmail.com>
2017-11-01 23:49:23 -0400

    added another solution to question Fiz  Buzz

--- leetpython.py
+++ leetpython.py
@@ -5074,7 +5074,9 @@ class Solution(object):
             else:
                 res.append(str(i))
         return res
-        
+    
+    def fizzBuzz(self, n):
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n + 1)]
 #------------- to do list -------------
 # 294. Flip Game ||
 class Solution(object):
