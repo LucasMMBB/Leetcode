84bc37c7b2a3071892075fd040f6302a77de9255
LucasMMBB <maoxummbb@gmail.com>
2017-11-01 23:44:23 -0400

    added one solution to question fizzBuzz

--- leetpython.py
+++ leetpython.py
@@ -5056,6 +5056,25 @@ class Solution(object):
 
         return self.helper(i + 1, n) + self.helper(i + 2, n)       
 
+# 412. Fizz Buzz
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                res.append("FizzBuzz")
+            elif i % 3 == 0 and i % 5 != 0:
+                res.append("Fizz")
+            elif i % 5 == 0 and i % 3 != 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+        return res
+        
 #------------- to do list -------------
 # 294. Flip Game ||
 class Solution(object):
