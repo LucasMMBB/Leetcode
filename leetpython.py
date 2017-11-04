5341fad1c10cfe697c99e58dbe3d9baa95353550
LucasMMBB <maoxummbb@gmail.com>
2017-11-04 19:54:20 -0400

    added one solution to question Invert Binary Tree

--- leetpython.py
+++ leetpython.py
@@ -5077,6 +5077,37 @@ class Solution(object):
     
     def fizzBuzz(self, n):
         return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n + 1)]
+
+
+# 226. Invert Binary Tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.helper(root)
+        return root
+        
+    def helper(self, node):
+        if node == None:
+            return node
+        
+        node.left, node.right = node.right, node.left
+        
+        self.helper(node.left)
+        self.helper(node.right)
+        
+
+
+
 #------------- to do list -------------
 # 294. Flip Game ||
 class Solution(object):
