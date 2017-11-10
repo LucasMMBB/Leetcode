5ee9aed1b1604adc1adf7cd6fa4d1fcfcc517ef4
LucasMMBB <maoxummbb@gmail.com>
2017-11-09 22:59:51 -0500

    added one solution to question First Unique in a string

--- leetpython.py
+++ leetpython.py
@@ -5310,7 +5310,28 @@ class Solution(object):
         
         return gap
 
+# 387. First Unique Character in a String
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        map = {}
+        for i in len(s):
+            if s[i] not in s:
+                map[s[i]] = 1
+            else
+                map[s[i]] += 1
 
+        for i in len(s):
+            if map[s[i]] == 1:
+                return i
+
+        return -1
+        
+
+        
 # ------------- TODO --------
 
 # 137. Single Number ||
